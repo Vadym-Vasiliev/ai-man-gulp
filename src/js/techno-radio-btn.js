cfb96a24325ebbf189a9a72335bfb8cf19b8df75
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-08-28 09:49:34 +0300

    add fix radioBtns techno

--- src/js/techno-radio-btn.js
+++ src/js/techno-radio-btn.js
@@ -1,22 +1,18 @@
-const radioBtns = document.querySelectorAll('[name="radio-btn"]');
-const radioContent = document.querySelectorAll(".techno-bottom");
+const radioButtons = document.querySelectorAll("[name='radio-btn']");
+const radioContents = document.querySelectorAll(".techno-bottom");
 
-function contentShow(index) {
-  console.log(index);
-  radioContent.forEach((content, contentIndex) => {
-    console.log(content);
-
-    if (index === contentIndex) {
-      content.classList.add("is-visible");
+function contentShow(indexButton) {
+  radioContents.forEach((radioContent, indexContent) => {
+    if (indexButton === indexContent) {
+      radioContent.classList.add("is-visible");
     } else {
-      content.classList.remove("is-visible");
+      radioContent.classList.remove("is-visible");
     }
   });
 }
 
-radioBtns.forEach((radioBtn, index) => {
-  radioBtn.addEventListener("change", () => {
-    console.log(radioBtn);
-    contentShow(index);
+radioButtons.forEach((radioButton, indexButton) => {
+  radioButton.addEventListener("change", () => {
+    contentShow(indexButton);
   });
 });
