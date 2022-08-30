8deeba09a7df1a4da5987c89dbcac8e7727e92fa
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-08-30 13:34:17 +0300

    add close  modal js

--- src/js/backdrop-modal.js
+++ src/js/backdrop-modal.js
@@ -2,10 +2,40 @@ const openModalBtn = document.querySelector("[data-modal-open]");
 const closeModalBtn = document.querySelector("[data-modal-close]");
 const modal = document.querySelector("[data-modal]");
 
-openModalBtn.addEventListener("click", toggleModal);
-closeModalBtn.addEventListener("click", toggleModal);
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+
+function windowModalRemoveEventListener() {
+  window.removeEventListener("keydown", closeModalEscape);
+  modal.removeEventListener("click", closeModalClickBackdrop);
+}
+
+function openModal() {
+  window.addEventListener("keydown", closeModalEscape);
+  modal.addEventListener("click", closeModalClickBackdrop);
+  toggleModal();
+}
+
+function closeModal() {
+  toggleModal();
+  windowModalRemoveEventListener();
+}
 
 function toggleModal() {
   modal.classList.toggle("backdrop__is-hidden");
   document.body.classList.toggle("modal-open");
+}
+
+function closeModalEscape(e) {
+  if (e.code === "Escape") {
+    toggleModal();
+    windowModalRemoveEventListener();
+  }
+}
+
+function closeModalClickBackdrop(e) {
+  if (e.target === e.currentTarget) {
+    toggleModal();
+    windowModalRemoveEventListener();
+  }
 }
